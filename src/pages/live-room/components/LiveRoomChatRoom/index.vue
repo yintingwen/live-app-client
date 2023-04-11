c8d78265b9d7d5ceb2240b6bc67477d95c85b230
yintingwen <yintingwen@foxmail.com>
2023-04-12 01:01:17 +0800

    chat

--- src/pages/live-room/components/LiveRoomChatRoom/index.vue
+++ src/pages/live-room/components/LiveRoomChatRoom/index.vue
@@ -1,85 +1,118 @@
 <template>
-  <list
-    class="live-room-chat-room" 
-		:fixFreezing="true"
-    ref="listRef"
-    @loadmore="onLoadMore"
+  <scroll-view
+    class="live-room-chat-room"
+    :scroll-into-view="`chat-${list.length - 1}`"
+    :scroll-y="true"
+    :show-scrollbar="false"
   >
-    <cell v-for="item in list" class="chat-item">
-      <image
-        src="/static/images/live-room/chat_room_level.png"
-        mode="scaleToFill"
-      />
-    </cell>
-  </list>
-
+    <view class="chat-item">
+      <view class="chat-item-content chat-sys-item">
+        <text class="chat-item-text chat-sys-text">
+          提倡绿色文明直播，提倡绿色文明直播，提倡绿色文明直播，提倡绿色文明直播，提倡绿色文明直播，提倡绿色文明直播，提倡绿色文明直播，提倡绿色文明直播。
+        </text>
+      </view>
+    </view>
+    <view v-for="(item, index) in list" :id="`chat-${index}`">
+      <view class="chat-item chat-user-item">
+        <view class="chat-level">
+          <view class="level-num">
+            <text class="level-text">{{ item.level }}</text>
+          </view>
+          <image class="level-icon" src="/static/images/live-room/chat_room_level.png" />
+        </view>
+        <view class="chat-item-content chat-user-item-text">
+          <text class="chat-item-text chat-sys-text">{{ item.name }}：</text>
+          <text class="chat-item-text chat-user-item-content">{{ item.text }}</text>
+        </view>
+      </view>
+    </view>
+  </scroll-view>
 </template>
 <script setup>
-import { ref, defineExpose, defineEmits } from 'vue'
+import { defineProps } from 'vue'
 
-defineExpose({
-  setSpecialEffects,
-  clearSetSpecialEffects
+defineProps({
+  list: { type: Array, default: [] },
 })
-
-const emit = defineEmits(['loadmore'])
-const listRef = ref(null)
-// const props = defineProps({
-//   list: { type: Array, default: () => [
-//     { text:'撒带教案数据库', level: 1 },
-//     { text: '12312312233', level: 2 }
-//   ]}
-// })
-
-const list = ref([
-    { text:'撒带教案数据库', level: 1 },
-    { text: '12312312233', level: 2 },
-    { text: '12312312233', level: 2 },
-    { text: '12312312233', level: 2 },
-    { text: '12312312233', level: 2 },
-    { text: '12312312233', level: 2 },
-    { text: '12312312233', level: 2 },
-    { text: '12312312233', level: 2 },
-    { text: '12312312233', level: 2 },
-    { text: '12312312233', level: 2 },
-    { text: '12312312233', level: 2 },
-    { text: '12312312233', level: 2 }
-])
-
-function setSpecialEffects (id) {
-  console.log(id);
-  listRef.value.setSpecialEffects({
-    id
-  })
-}
-
-function clearSetSpecialEffects () {
-  listRef.value.setSpecialEffects({
-    id: ''
-  })
-  console.log('clear');
-}
-
-function onLoadMore (e) {
-  emit('loadmore', e)
-
-}
 </script>
 <style lang="scss">
 .live-room-chat-room {
-  background-color: #fff;
+  position: fixed;
   width: 536rpx;
-  height: 520rpx;
+  max-height: 520rpx;
   bottom: 168rpx;
   left: 24rpx;
-  z-index: 1;
-  display: flex;
+  align-items: flex-start;
 
   .chat-item {
-		width: 200rpx;
-		text-align: center;
-		font-size: 36rpx;
     background-color: rgba(0, 0, 0, 0.3);
+    margin-top: 8rpx;
+    border-radius: 20rpx;
+    padding-left: 12rpx;
+    padding-right: 24rpx;
+    padding-top: 8rpx;
+    padding-bottom: 8rpx;
+
+    .chat-item-text {
+      line-height: 42rpx;
+      font-size: 26rpx;
+    }
+  }
+
+  .chat-item-content {
+  }
+
+  .chat-sys-text {
+    color: #75dfff;
+    font-size: 26rpx;
+  }
+
+  .chat-user-item {
+    display: flex;
+    flex-direction: row;
+
+    .chat-user-item-text {
+      @extend .chat-user-item;
+
+      .chat-user-item-content {
+        color: #fff;
+      }
+    }
+  }
+
+  .chat-level {
+    position: relative;
+    height: 42rpx;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    padding-left: 2rpx;
+    margin-right: 12rpx;
+
+    .level-icon {
+      height: 42rpx;
+      width: 42rpx;
+      position: absolute;
+      left: -5rpx;
+      z-index: 1;
+    }
+
+    .level-num {
+      border-radius: 13rpx;
+      height: 26rpx;
+      width: 74rpx;
+      background-color: #1e6ff5;
+      border-top-left-radius: 13rpx;
+      border-top-right-radius: 13rpx;
+      border-bottom-left-radius: 13rpx;
+      border-bottom-right-radius: 13rpx;
+      padding-left: 26rpx;
+
+      .level-text {
+        text-align: center;
+        color: #fff;
+      }
+    }
   }
 }
 </style>
---
+++ src/pages/live-room/components/LiveRoomCover/index.vue
@@ -0,0 +1,20 @@
+<template lang="">
+  <image
+    mode="aspectFill"
+    src="https://profile.csdnimg.cn/D/5/0/1_luoluoyang23"
+    class="live-room-cover"
+  ></image>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss">
+.live-room-cover {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+}
+</style>
